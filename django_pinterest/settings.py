1d908cc43abc1683a50ec53b39bbd09dce8d9dbe
hojunkimdev <hojunkim.dev@gmail.com>
2021-08-13 16:41:25 +0900

    Add base.css

--- django_pinterest/settings.py
+++ django_pinterest/settings.py
@@ -131,6 +131,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_FILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
