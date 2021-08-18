4fd42000965b096704e219b99688f692d0987142
hojunkimdev <hojunkim.dev@gmail.com>
2021-08-18 16:34:40 +0900

    Bug fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -39,6 +39,7 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
@@ -46,5 +47,6 @@ class AccountUpdateView(UpdateView):
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
