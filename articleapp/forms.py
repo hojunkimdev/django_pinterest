4c8acd0f25eddc3bbbbbc76926abe16aa27e38a2
hojunkimdev <hojunkimdev@gmail.com>
2021-11-04 10:25:06 +0900

    Add WYSIWYG to Articleapp

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    # for customize WYSIWYG
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto; text-align: left;'}))
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
