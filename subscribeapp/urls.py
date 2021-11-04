82bec274839636ef592c59d8d140408eff3dfdf0
hojunkimdev <hojunkimdev@gmail.com>
2021-11-04 09:56:46 +0900

    Add Subscribeapp - list

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from subscribeapp.views import SubscriptionView
+from subscribeapp.views import SubscriptionView, SubscriptionListView
 
 app_name = "subscribeapp"
 
 urlpatterns = [
     path('subscribe/', SubscriptionView.as_view(), name='subscribe'),
+    path('list/', SubscriptionListView.as_view(), name='list'),
 ]
