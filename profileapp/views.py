8ea9a2d672d8f41273aa4df086917d561bf7a49a
hojunkimdev <hojunkimdev@gmail.com>
2021-11-12 10:07:14 +0900

    Bug fix

--- profileapp/views.py
+++ profileapp/views.py
@@ -14,7 +14,7 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    # success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -34,7 +34,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    # success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
