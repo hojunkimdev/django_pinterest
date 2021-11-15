5d65535322bdc4adf93cb47d64c5758979086951
hojunkimdev <hojunkimdev@gmail.com>
2021-11-15 10:23:20 +0900

    Update likeapp - transaction

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
@@ -12,6 +13,17 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    if LikeRecord.objects.filter(user=user, article=article).exists():
+        raise ValueError('Like already exists')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
@@ -21,14 +33,11 @@ class LikeArticleView(RedirectView):
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
 
-        if LikeRecord.objects.filter(user=user, article=article).exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValueError:
             messages.add_message(self.request, messages.ERROR, '좋아요는 한번만 가능합니다.')
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': article['pk']}))
 
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
